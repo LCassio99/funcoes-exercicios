dd7a0efb7bd1638bf1cc1261c0f6af762560de46
Luciano <lucianocassio66@gmail.com>
2023-05-09 20:23:01 -0300

    exercicio3

---
+++ exercicio3/script.js
@@ -0,0 +1,45 @@
+const soma = (valor1,valor2) =>{
+
+    const somatorio = valor1 + valor2
+
+    return somatorio
+
+}
+
+console.log(soma(30,20))
+
+//-----------------------------------------------
+
+const subtracao = (valor1,valor2) =>{
+
+    const resultado = valor1 - valor2
+
+    return resultado
+
+}
+
+console.log(subtracao(30,20))
+
+//-----------------------------------------------
+
+const multiplica = (valor1,valor2) =>{
+
+    const produto = valor1 * valor2
+
+    return produto
+
+}
+
+console.log(multiplica(30,20))
+
+//-----------------------------------------------
+
+const divisao = (valor1,valor2) =>{
+
+    const resto = valor1 / valor2
+
+    return resto
+
+}
+
+console.log(divisao(30,20))
